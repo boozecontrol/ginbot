997a044c8fcd1955b340e3d7fe1520676714c741
Jack <git@jackhiggins.ie>
2019-06-30 12:12:38 +0200

    reversal of vcc and gnd and working rotary behind multiplexer

--- rotary_multiplexer_testing/rotary_multiplexer_testing.ino
+++ rotary_multiplexer_testing/rotary_multiplexer_testing.ino
@@ -1,42 +1,66 @@
-#define inputA 6
-#define inputB 7
+#define MULTIPLEXER_SIGNAL 12
 
-#define button 12
+#define MULTIPLEXER_S0 15
+#define MULTIPLEXER_S1 13
+#define MULTIPLEXER_S2 2
+#define MULTIPLEXER_S3 16
+
+int MULTIPLEX_PINS[] = {MULTIPLEXER_S0, MULTIPLEXER_S1, MULTIPLEXER_S2, MULTIPLEXER_S3};
+
+#define ROTARY_BUTTON 12
+#define ROTARY_CLK 14
+#define ROTARY_DT 13
+
+//  Arduino | NodeMCU | Used
+//  0       | D3     
+//  1       | TX     
+//  2       | D4      | M_S2
+//  3       | RX     
+//  4       | D2     
+//  5       | D1    
+//  6       |
+//  7       | 
+//  8       |      
+//  9       | S2
+//  10      | S3
+//  11      |    
+//  12      | D6      | M_SIG
+//  13      | D7      | M_S1
+//  14      | D5      | 
+//  15      | D8      | M_S0
+//  16      | D0      | M_S3
 
 int prevCounter = 0;
-int counter = 0;
-int aState;
-int aLastState;
-
+long counter = 0;
+boolean aState;
+boolean aLastState;
 boolean buttonState = false;
 
-void setup(){
 
-pinMode(inputA, INPUT);
-pinMode(inputB, INPUT);
-pinMode(button, INPUT);
+void setup() {
+  pinMode(MULTIPLEXER_SIGNAL, INPUT);
 
-Serial.begin(9600);
+  pinMode(MULTIPLEXER_S0, OUTPUT);
+  pinMode(MULTIPLEXER_S1, OUTPUT);
+  pinMode(MULTIPLEXER_S2, OUTPUT);
+  pinMode(MULTIPLEXER_S3, OUTPUT);
 
-aLastState = digitalRead(inputA);
-  
+  Serial.begin(9600);
+
+  aLastState = rotaryA();
 }
 
-
-void loop(){
-
-  aState = digitalRead(inputA);
+void loop() {
+  aState = rotaryA();
 
 
   if(aState != aLastState){
-    if(digitalRead(inputB) != aState) {
+    if(rotaryB() != aState) {
       counter++;
       
     }else{
       counter--;
     }
-
-Serial.println(counter);
 
     if(counter > prevCounter){
       Serial.println("Next");
@@ -48,12 +72,10 @@ Serial.println(counter);
 
     prevCounter= counter;
 
-//    Serial.print("Position:");
-//    Serial.println(counter);
     delay(200);
   }
 
-  boolean buttonPress = !digitalRead(button);
+  boolean buttonPress = rotaryButton();
 
   if(buttonPress){
     
@@ -71,4 +93,31 @@ Serial.println(counter);
   }
 
   aState = aLastState;
+}
+
+boolean rotaryButton(){
+  setMultiplex(ROTARY_BUTTON);
+  return digitalRead(MULTIPLEXER_SIGNAL);
+}
+
+boolean rotaryA(){
+  setMultiplex(ROTARY_CLK);
+  return digitalRead(MULTIPLEXER_SIGNAL);
+}
+
+boolean rotaryB(){
+  setMultiplex(ROTARY_DT);
+  return digitalRead(MULTIPLEXER_SIGNAL);
+}
+
+void setMultiplex(int activeChannel){
+  for(int b=0; b<4; b++){
+    boolean result = bitRead(activeChannel, b);
+
+    if(result){
+      digitalWrite(MULTIPLEX_PINS[b], HIGH);
+    }else{
+      digitalWrite(MULTIPLEX_PINS[b], LOW);
+    }
+  }
 }
